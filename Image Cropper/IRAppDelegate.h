99e4ef168c73fa76aedc69083f4ab9a46783f931
Igor Rubanau <Igor_Rubanau@epbyminw3624.minsk.epam.com>
2012-09-03 17:19:50 +0300

    Initial Commit

---
+++ Image Cropper/IRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  IRAppDelegate.h
+//  Image Cropper
+//
+//  Created by Igor Rubanau on 9/3/12.
+//  Copyright (c) 2012 Igor Rubanau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Image Cropper/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Image Cropper
+//
+//  Created by Igor Rubanau on 9/3/12.
+//  Copyright (c) 2012 Igor Rubanau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "IRAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([IRAppDelegate class]));
+    }
+}
